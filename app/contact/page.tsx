40d94798994dde1e94757d3b645d1fc87ea14d56
Joseph Doba <josephdoba@gmail.com>
2025-12-07 18:34:34 -0800

    connect navbar pages

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,10 +1,10 @@
-export default function About() {
+export default function Contact() {
   return (
     <div className="flex min-h-screen items-center justify-center">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 sm:items-start">
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-4xl leading-1 tracking-tight font-sans">
-            Who Am I?
+            Contact Me
           </h1>
           <p className="text-medium font-mono">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer rutrum in dui non convallis. Ut consequat facilisis sapien vitae lobortis. Proin tempor, sem ac posuere feugiat, lectus turpis hendrerit elit, eget tempor nulla elit a urna. In pellentesque turpis turpis, nec pulvinar libero imperdiet et. Maecenas scelerisque malesuada eros at vehicula. Fusce dapibus volutpat mauris, sit amet posuere dui blandit at. Cras ac pretium felis, ac convallis ante. Donec et nulla a velit tristique suscipit. Donec est sapien, convallis tincidunt lacinia nec, pellentesque porta eros. Mauris sit amet lectus lorem. Vestibulum urna urna, tincidunt in bibendum eu, condimentum id tortor. Suspendisse mattis iaculis magna eget dictum. Proin at enim nec odio ullamcorper dignissim. Quisque venenatis, ligula eu pharetra maximus, sapien odio faucibus magna, at accumsan purus odio id lacus. Sed sit amet ante nec tellus imperdiet fringilla sed ut nibh.

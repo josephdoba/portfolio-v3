ec9a7b1d14f9f0b0fa010b6fd60aa862a499f7ef
Joseph Doba <josephdoba@gmail.com>
2025-12-08 21:01:03 -0800

    style home page

--- components/BlogLatest.tsx
+++ components/BlogLatest.tsx
@@ -1,17 +1,17 @@
 export default function BlogLatest () {
   return (
-<div className=" items-center justify-center bg-zinc-90 font-sans border-1 border-solid border-[var(--color-red)]">
-      <section className=" w-full max-w-3xl flex-col items-center justify-between py-32 px-16 sm:items-start">
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left ">
-          {/* <h1 className="max-w-xs text-4xl leading-1 tracking-tight">
-            Joseph AJ Doba
-          </h1>
-          <h2 className="text-lg font-sans">
-            Full-Stack Web Developer and Cyber Security Specialist.
-          </h2>
-          <p className="text-medium font-mono">
-            Hello! I’m a freelance developer who builds and secures websites for small and medium-sized businesses.
-          </p> */}
+<div className="items-center justify-center font-sans border-1 border-solid border-[var(--color-red)]">
+      <section className="w-full px-16">
+         {/* max-w-3xl flex-col items-center justify-between py-32 sm:items-start */}
+        <h1 className="text-3xl leading-1 tracking-tight">
+            Blog
+        </h1>
+          <ul className="grid gap-6 md:grid-cols-2 items-stretch p-[15%]">
+            <li className="h-full">Latest blog post</li>
+          </ul>
+        <div className="max-w-5xl mx-auto px-6 font-sans ">
+          {/* flex flex-col items-center gap-6 text-center sm:items-start sm:text-left */}
+          
         </div>
       </section>
     </div >
--- components/ServicesSection.tsx
+++ components/ServicesSection.tsx
@@ -2,8 +2,8 @@ import ServicesCard from "./ServicesCard"
 
 export default function ServicesSection () {
   return (
-<div className="items-center justify-center font-sans border-1 border-solid border-[var(--color-red)]">
-      <section className="w-full px-16">
+<div className="items-center justify-center font-sans border-1 border-solid border-[var(--color-red)] bg-(--color-alt-white)">
+      <section className="w-full px-16 ">
          {/* max-w-3xl flex-col items-center justify-between py-32 sm:items-start */}
         <h1 className="text-3xl leading-1 tracking-tight">
             Services
@@ -20,19 +20,3 @@ export default function ServicesSection () {
     </div >
   )
 }
-
-
-    // <section className="w-full py-16">
-    //   <div className="max-w-5xl mx-auto px-6 font-sans">
-    //     <h2 className="text-4xl tracking-tight mb-8">
-    //       Services
-    //     </h2>
-
-    //     <ul className="grid gap-6 md:grid-cols-2 items-stretch">
-    //       <li className="h-full">
-    //         <ServicesCard />
-    //       </li>
-    //       <li className="h-full">
-    //         <ServicesCard />
-    //       </li>
-

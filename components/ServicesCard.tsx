e6ce597920ed1cea8d11fdb8499b823b0425c8d0
Joseph Doba <josephdoba@gmail.com>
2025-12-09 17:40:13 -0800

    add subtle svb background

--- components/ServicesCard.tsx
+++ components/ServicesCard.tsx
@@ -1,8 +1,8 @@
 export default function ServicesCard () {
   return (
-<div className=" items-center justify-center bg-zinc-90 font-sans border-1 border-solid bg-(--color-white)">
-      <section className="w-full max-w-3xl flex-col items-center justify-between py-24 px-16 sm:items-start">
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
+<div className=" items-center justify-center bg-zinc-90 font-sans drop-shadow">
+      <section className="w-full max-w-3xl flex-col items-center justify-between py-24 px-16 sm:items-start border-1">
+        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left drop-shadow-lg">
           <h1 className="max-w-xs text-4xl leading-1 tracking-tight">
             card.
           </h1>
